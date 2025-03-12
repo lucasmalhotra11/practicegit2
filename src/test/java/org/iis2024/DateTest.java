c8900d9366e19d62d989ac6ade853b6aced71294
Loic <loicd@bell.net>
2025-03-12 14:05:36 +0100

    Collaborator commit

--- src/test/java/org/iis2024/DateTest.java
+++ src/test/java/org/iis2024/DateTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 /** Unit tests for the Date class. */
 public class DateTest {
 
-  private int
+  private int fieldAddedByCollaborator
 
   private final int day;
 
